6c230530c1e6a258900c2e206d279d506ae85a98
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 18:09:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var itemConstructor = {};
-  itemConstructor[itemName] = name;
+  var itemConstructor = { itemName : 'itemName' , price : 'itemPrice' }
+  itemConstructor[itemName] = item;
   itemConstructor[price] = Math.floor(Math.random()*101)+1;
 
   return itemConstructor;
