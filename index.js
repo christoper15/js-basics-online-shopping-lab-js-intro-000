76f1635871bbbc791f4afe52812a7e69842b38d9
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 22:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function placeOrder(cardNumber) {
     if (cardNumber == undefined) {
       return "Sorry, we don't have a credit card on file for you.";
     } else {
-      cart.splice{ 0 , cart.length - 1 }
+      cart.splice(0,cart.length - 1)
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     }
 }
