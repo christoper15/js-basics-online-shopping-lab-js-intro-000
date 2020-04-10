d92a1d4c633fcad144929bbff13bf304b281250f
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-10 20:04:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
 
 function viewCart() {
   var string = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
-  if (cart.length == 0) {
+  if (cart.length === 0) {
     return 'Your shopping cart is empty.';
   } else {
     let i = 0;
